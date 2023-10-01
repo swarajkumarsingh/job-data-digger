f0264f8bd01c47e464c796f51456f1e07418f63f
swaraj <sswaraj169@gmail.com>
2023-10-01 20:50:10 +0530

    added errorCodes package

---
+++ errorCodes/errorCodes.go
@@ -0,0 +1,10 @@
+// package errorcodes - contains useful status codes
+package errorcodes
+
+import "net/http"
+
+const STATUS_OK = http.StatusOK
+const STATUS_CREATED = http.StatusCreated
+const STATUS_NOT_FOUND = http.StatusNotFound
+const STATUS_BAD_REQUEST = http.StatusBadRequest
+const STATUS_INTERNAL_SERVER_ERROR = http.StatusInternalServerError
